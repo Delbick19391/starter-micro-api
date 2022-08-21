548b39a5ee5b68707956bb08af000ddca489dfdf
Delbick19391 <111655140+Delbick19391@users.noreply.github.com>
2022-08-21 22:12:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Server started")
+const mineflayer = require('mineflayer');
+function createBot()
+{
+  let bot = mineflayer.createBot({
+    host: '46.4.94.213',
+    username: "SlimeShield_BOT",
+    port: 38302,
+    version: "1.18.2"
+        })
+        
+        //bot.on('error', (err) => console.log(err))
+        //bot.on('end', createBot)
+    
+        bot.once('spawn', () =>{
+            bot.chat("/login dk19kaZo3mA")
+        })
+    
+    }
+    createBot()
+
+
+
